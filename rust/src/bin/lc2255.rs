95947ac50c4d1e50d3da268dd6f02c8cf7817d78
zhichen.Wang <wzcboss@qq.com>
2022-06-12 20:49:46 +0800

    add lc2255

--- rust/src/bin/lc2255.rs
+++ rust/src/bin/lc2255.rs
@@ -1,3 +1,6 @@
+#![feature(pattern)]
+use std::str::pattern::Pattern;
+
 struct Solution;
 impl Solution {
     pub fn count_prefixes(words: Vec<String>, s: String) -> i32 {
@@ -19,11 +22,20 @@ impl Solution {
 
         ans
     }
+    pub fn count_prefixes1(words: Vec<String>, s: String) -> i32 {
+        let mut ans = 0;
+        words.iter().for_each(|x| {
+            if x.is_prefix_of(&s) {
+                ans += 1;
+            }
+        });
+        ans
+    }
 }
 
 fn main() {
     let mut input1 = vec!["a", "b", "c", "ab", "bc", "abc"];
     let input1 = input1.iter_mut().map(|x| x.to_string()).collect();
 
-    println!("{}", Solution::count_prefixes(input1, "abc".to_string()));
+    println!("{}", Solution::count_prefixes1(input1, "abc".to_string()));
 }
